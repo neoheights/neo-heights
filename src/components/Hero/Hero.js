3afdbe2b3e8ec33114aef3da0aeaf8bcb6d0ed69
Naveenprasanth T <102594021+NaveenprasanthT@users.noreply.github.com>
2026-07-18 20:30:15 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -37,7 +37,7 @@ const Hero = () => {
             <div className={styles.overlayContainer}></div>
             <div className={styles.overlay}>
                 <video
-                    src="/videos/neo_heights.mp4"
+                    src="/videos/intro.mkv"
                     autoPlay
                     muted
                     loop
