9e7ccb4a3a27b4cb5fc1e8c270a4624c566b2180
kathiravan-runatec <kathiravan.s@runatechnology.com>
2026-07-18 20:30:24 +0530

    ddd

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -37,7 +37,7 @@ const Hero = () => {
             <div className={styles.overlayContainer}></div>
             <div className={styles.overlay}>
                 <video
-                    src="/videos/neo_heights.mp4"
+                    src="/videos/neo_123.mp4"
                     autoPlay
                     muted
                     loop
